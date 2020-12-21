17b0249f6424d719b69d362e2ba9baae47de928f
CycLooops <55794284+CycLooops@users.noreply.github.com>
2020-12-21 23:17:30 +0800

    Add files via upload
    First version.

---
+++ test111/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace test111
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(138, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(189, 25);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "请输入姓名";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(138, 105);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(189, 25);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "请输入学号";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(138, 150);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(189, 25);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "请输入密码";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(138, 206);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(189, 25);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Text = "请输入寝室号";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteCustomSource.AddRange(new string[] {
+            "中心校区"});
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "中心校区",
+            "南岭校区",
+            "南湖校区",
+            "和平校区",
+            "朝阳校区",
+            "新民校区"});
+            this.comboBox1.Location = new System.Drawing.Point(138, 295);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(189, 23);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Text = "请选择校区";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "北苑1公寓",
+            "北苑2公寓",
+            "南苑1公寓",
+            "南苑2公寓",
+            "南苑3公寓"});
+            this.comboBox2.Location = new System.Drawing.Point(138, 346);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(189, 23);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.Text = "请选择寝室楼";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 389);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "打卡！";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(138, 252);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(189, 25);
+            this.textBox5.TabIndex = 9;
+            this.textBox5.Text = "请输入班级（两位）";
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(411, 112);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(55, 25);
+            this.textBox6.TabIndex = 10;
+            this.textBox6.Text = "09";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(509, 112);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(55, 25);
+            this.textBox7.TabIndex = 11;
+            this.textBox7.Text = "00";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(509, 222);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(55, 25);
+            this.textBox8.TabIndex = 13;
+            this.textBox8.Text = "00";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(411, 222);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(55, 25);
+            this.textBox9.TabIndex = 12;
+            this.textBox9.Text = "21";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(472, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 15);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(472, 222);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 15);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(408, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(172, 15);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "请输入早上打卡开始时间";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(408, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(172, 15);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "请输入晚上打卡开始时间";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(411, 293);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(189, 25);
+            this.textBox10.TabIndex = 18;
+            this.textBox10.Text = "早上打卡等待";
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(411, 344);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(189, 25);
+            this.textBox11.TabIndex = 19;
+            this.textBox11.Text = "晚上打卡等待";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 450);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ test111/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using test111;
+
+namespace test111
+{
+    public partial class Form1 : Form
+    {
+        static int cnt=0;
+        static int campus, dormitory;
+        public static int curh, nonh, curm, nonm, flg1, ngth, ngtm, flg2;
+        static string path = @".\user.json";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int flg1 = 0, flg2 = 0;
+            String s="设置成功!\n";
+            int curh = Convert.ToInt32(DateTime.Now.Hour.ToString());
+            int curm = Convert.ToInt32(DateTime.Now.Minute.ToString());
+            int cum = curh * 60 + curm;
+            int nonh = Convert.ToInt32(textBox6.Text.Substring(0, 2));
+            int nonm = Convert.ToInt32(textBox7.Text.Substring(0, 2));
+            int nonc = nonh * 60 + nonm;
+            int ngth = Convert.ToInt32(textBox9.Text.Substring(0, 2));
+            int ngtm = Convert.ToInt32(textBox8.Text.Substring(0, 2));
+            int ngtc = ngth * 60 + ngtm;
+            if (nonc < cum) {
+                s += "早上打卡时间已过\n";
+            }
+            else
+            {
+                s += "距离早上打卡还剩" + (int)(nonc - cum) + "分钟\n";
+            }
+            if (ngtc < cum)
+            {
+                s += "晚上打卡时间已过\n";
+            }
+            else
+            {
+                s += "距离晚上打卡还剩" + (int)(ngtc - cum) + "分钟\n";
+            }
+                       MessageBox.Show(s);
+            //            MessageBox.Show("早打卡已完成!");
+            //           textBox10.Text = "早打卡已完成于 " + nonh + " : " + nonm;
+            FileStream fs = new FileStream(path, FileMode.Create);
+            fs.Close();
+            using (StreamWriter file = new StreamWriter(path, true))
+            {
+
+                file.WriteLine("{");
+                file.WriteLine("\"transaction\": \"BKSMRDK\",");
+                file.WriteLine("\"users\": [");
+                file.WriteLine("{");
+                file.WriteLine("\"username\":\"" + textBox1.Text + "\",");
+                file.WriteLine("\"password\":\"" + textBox3.Text + "\",");
+                file.WriteLine("\"fields\":{");
+                file.WriteLine("\"fieldSQxq\": \"" + campus + "\",");
+                file.WriteLine("\"fieldSQgyl\": \"" + dormitory + "\",");
+                file.WriteLine("\"fieldSQqsh\":\"" + textBox4.Text + "\",");
+                file.WriteLine("\"fieldSQnj\":\"" + textBox1.Text.Substring(textBox1.Text.Length-4,4) + "\",");
+                file.WriteLine("\"fieldSQnj_Name\":\"20" + textBox1.Text.Substring(textBox1.Text.Length - 2, 2) + "\",");
+                int x = 880 + Convert.ToInt32(textBox4.Text) - 26;
+                file.WriteLine("\"fieldSQbj\": \"" + x + "\",");
+                file.WriteLine("\"fieldSQbj_Name\":\"" + textBox1.Text.Substring(textBox1.Text.Length - 4, 4) + textBox5.Text + "\"");
+                file.WriteLine("}");
+                file.WriteLine("}");
+                file.WriteLine("]");
+                file.WriteLine("}");
+                file.Close();
+            }
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            cnt++;
+            if (cnt == 10)
+            {
+                
+                cnt = 0;
+                if (DateTime.Now.Hour == nonh && DateTime.Now.Minute == nonm && flg1 == 0)
+                    {
+                    try
+                    {
+                        Class1.daka();
+                        if (flg1 == 1)
+                        {
+                            MessageBox.Show("早打卡已完成！");
+                            textBox10.Text = "早打卡完成于" + DateTime.Now.Hour.ToString() + ":" + DateTime.Now.Minute.ToString();
+                        }
+                    }
+                    catch(Exception ex)
+                    {
+                        textBox10.Text = ex.ToString();
+                    }
+                }
+                if (DateTime.Now.Hour == ngth && DateTime.Now.Minute == ngtm && flg2 == 0)
+                    {
+                    try
+                    {
+                         Class1.daka();
+                        if (flg2 == 1)
+                        {
+                            MessageBox.Show("晚打卡已完成！");
+                            textBox10.Text = "晚打卡完成于" + DateTime.Now.Hour.ToString() + ":" + DateTime.Now.Minute.ToString();
+                        }
+                    }
+                    catch(Exception ex)
+                    {
+                        textBox11.Text = ex.ToString();
+                    }
+                }
+                
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedItem.ToString()) //获取选择的内容
+            {
+
+                case "中心校区": campus = 1; break;
+                case "南岭校区": campus = 2; break;
+                case "新民校区": campus = 3; break;
+                case "南湖校区": campus = 4; break;
+                case "和平校区": campus = 5; break;
+                case "朝阳校区": campus = 6; break;
+                case "前卫北区": campus = 7; break;
+
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox2.SelectedItem.ToString()) //获取选择的内容
+            {
+
+                case "北苑1公寓": dormitory = 1; break;
+                case "北苑2公寓": dormitory = 2; break;
+                case "南苑1公寓": dormitory = 3; break;
+                case "南苑2公寓": dormitory = 4; break;
+                case "南苑3公寓": dormitory = 5; break;
+                case "南苑4公寓": dormitory = 6; break;
+                case "南苑5公寓A区": dormitory = 7; break;
+                case "南苑5公寓B区": dormitory = 8; break;
+                case "南苑5公寓C区": dormitory = 9; break;
+
+            }
+        }
+    }
+}
